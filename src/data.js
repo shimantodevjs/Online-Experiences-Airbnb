ff63c53450c021ce3818074f713f0fd4f0ba8d4f
unknown <shimantodevjs@gmail.com>
2023-03-21 15:39:29 +0300

    final commit

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export default [
         id: 1,
         title: "Life Lessons with rafael",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
-        price: 136,
+        price: 4136,
         coverImg: "work.png",
         stats: {
             rating: 5.0,
@@ -16,7 +16,7 @@ export default [
         id: 2,
         title: "Learn Wedding photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-        price: 125,
+        price: 3125,
         coverImg: "photographer.png",
         stats: {
             rating: 5.0,
@@ -29,7 +29,7 @@ export default [
         id: 3,
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
+        price: 5078,
         coverImg: "mountain.jpg",
         stats: {
             rating: 4.8,
@@ -40,38 +40,38 @@ export default [
     },
       {
         id: 4,
-        title: "Group Mountain Biking",
+        title: "Painting Lesson",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "mountain.jpg",
+        price: 7090,
+        coverImg: "painter.webp",
         stats: {
-            rating: 4.8,
-            reviewCount: 2
+            rating: 4.9,
+            reviewCount: 18
         },
-        location: "Norway",
+        location: "France",
         openSpots: 3,
     },
       {
         id: 5,
-        title: "Group Mountain Biking",
+        title: "Rock On With Ryan",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "mountain.jpg",
+        price: 5990,
+        coverImg: "guiter.jpeg",
         stats: {
-            rating: 4.8,
-            reviewCount: 2
+            rating: 4.5,
+            reviewCount: 7
         },
-        location: "Norway",
+        location: "Online",
         openSpots: 3,
     },
       {
         id: 6,
-        title: "Group Mountain Biking",
+        title: "Be the best cook",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "mountain.jpg",
+        price: 1050,
+        coverImg: "cook.webp",
         stats: {
-            rating: 4.8,
+            rating: 4.1,
             reviewCount: 2
         },
         location: "Online",
@@ -79,15 +79,15 @@ export default [
     },
       {
         id: 7,
-        title: "Group Mountain Biking",
+        title: "Learn Chinesse and Culture",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "mountain.jpg",
+        price: 590,
+        coverImg: "chinese.jpeg",
         stats: {
-            rating: 4.8,
-            reviewCount: 2
+            rating: 4.3,
+            reviewCount: 209
         },
-        location: "Norway",
-        openSpots: 3,
+        location: "China",
+        openSpots: 0,
     }
 ]
